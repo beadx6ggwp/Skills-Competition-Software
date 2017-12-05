2c785af9f9bb2313bf51c436b2834c7b4a4b9769
datadavid4 <32480038+datadavid4@users.noreply.github.com>
2017-12-05 19:08:00 +0800

    update

---
+++ 106工科賽/ANS/許展維_Q4-OK/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace 許展維_Q4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "讀取訓練資料";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "機器人訓練資料對\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 104);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 167);
+            this.textBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(241, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 109);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "訓練參數";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 78);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(144, 22);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "1.0;-1.0;0.0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "初始權重";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 50);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "0.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "學習率";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "9999";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "最大訓練次數";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "開始訓練";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(330, 249);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(144, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(247, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "訓練後的權重";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(555, 89);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(186, 54);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "測試輸入命令";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(89, 22);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(82, 22);
+            this.textBox8.TabIndex = 1;
+            this.textBox8.Text = "1.0;1.0;1.0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "新命令";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "測試";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(561, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "機器人向:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(623, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(2, 14);
+            this.label7.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 291);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ 106工科賽/ANS/許展維_Q4-OK/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 許展維_Q4
+{
+    public partial class Form1 : Form
+    {
+        int K = 0;// 訓練資料數
+        int N = 3;// 訓練參數量
+        double n = 0;// 學習率
+        double maxTime = 0;// 最大學習次數
+
+        List<double[]> W = new List<double[]>();
+        List<double[]> X = new List<double[]>();
+
+        List<double> Y = new List<double>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Directory.GetCurrentDirectory();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadData();
+
+            for (int i = 0; i < maxTime; i++)
+            {
+                if (learn() == 0) break;
+            }
+
+            textBox5.Text = string.Join(";", Array.ConvertAll(W[W.Count - 1], s => $"{s:0.00}"));
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double[] newX = Array.ConvertAll(textBox8.Text.Split(';'), double.Parse);
+            double output = O(F(W[W.Count - 1], newX));
+            label7.Text = output.ToString() + "," + (output > 0 ? "左" : "右");
+        }
+
+        double learn()
+        {
+            double err = 0;
+            for (int k = 1; k <= K; k++)
+            {
+                double f = F(W[W.Count - 1], X[k]);
+                double o = O(f);
+
+                if (o != Y[k])
+                {
+                    double[] newW = ReW(W[W.Count - 1], X[k], n, Y[k], o);
+                    W.Add(newW);
+                }
+                err = err + Math.Pow(Y[k] - o, 2) * 0.5;
+            }
+
+            return err;
+        }
+
+        double F(double[] w_arr, double[] x_arr)
+        {
+            double val = 0;
+            for (int i = 0; i < x_arr.Length; i++)
+            {
+                val += w_arr[i] * x_arr[i];
+            }
+            return val;
+        }
+        double O(double val)
+        {
+            return Math.Sign(val);
+        }
+
+        double[] ReW(double[] w_arr, double[] x_arr, double n, double y, double o)
+        {
+            double[] newW = new double[w_arr.Length];
+            double mult = n * (y - o);
+
+            for (int i = 0; i < newW.Length; i++)
+            {
+                newW[i] = w_arr[i] + mult * x_arr[i];
+            }
+
+            return newW;
+        }
+
+        void ReadData()
+        {
+            // 讀取訓練測資
+            string[] input = textBox1.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            K = input.Length;
+            n = double.Parse(textBox3.Text);
+            maxTime = double.Parse(textBox2.Text);
+
+            X.Add(new double[N]);
+            Y.Add(0);
+            for (int i = 0; i < input.Length; i++)
+            {
+                double[] temp = Array.ConvertAll(input[i].Split('\t'), double.Parse);
+                double[] x = new double[temp.Length - 1];
+
+                for (int j = 0; j < x.Length; j++) x[j] = temp[j];
+
+                X.Add(x);
+                Y.Add(temp[temp.Length - 1]);
+            }
+
+            // 設定初始權重
+            W.Add(Array.ConvertAll(textBox4.Text.Split(';'), double.Parse));
+        }
+
+    }
+}
