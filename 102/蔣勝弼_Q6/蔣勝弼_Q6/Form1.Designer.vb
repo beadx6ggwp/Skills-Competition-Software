36dd17d260df06f212f7d01e65b08c2d11d56cca
datadavid4 <32480038+datadavid4@users.noreply.github.com>
2017-12-13 16:34:38 +0800

    update

---
+++ 102/蔣勝弼_Q6/蔣勝弼_Q6/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "請輸入有幾個矩陣相乘? "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(151, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(82, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(173, 12)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "請輸入m1~m3的矩陣大小<維度>"
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.AutoScrollMinSize = New System.Drawing.Size(10, 10)
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Location = New System.Drawing.Point(12, 60)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(350, 325)
+        Me.Panel1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(298, 432)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "確認"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 419)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 12)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "最少的乘法運算次數: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 394)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 12)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "矩陣相乘的次序為: "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 460)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "多矩陣相乘"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ 102/蔣勝弼_Q6/蔣勝弼_Q6/Form1.vb
@@ -0,0 +1,126 @@
+﻿Public Class Form1
+    Dim lblText(9) As Label
+    Dim box(1, 9) As TextBox
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i = 0 To 9
+            For q = 0 To 1
+                box(q, i) = New TextBox
+                With box(q, i)
+                    .Location = New Point(160 + q * 60, 10 + i * 30)
+                    .Size = New Size(50, 22)
+                    .Visible = False
+                End With
+                Panel1.Controls.Add(box(q, i))
+            Next
+            lblText(i) = New Label()
+            With lblText(i)
+                .Location = New Point(10, 15 + i * 30)
+                .Text = "m" & (i + 1) & "的矩陣大小: "
+                .Visible = False
+            End With
+            Panel1.Controls.Add(lblText(i))
+        Next
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim index As Integer = Val(TextBox1.Text)
+        If index >= 3 And index <= 10 Then
+            index -= 1
+            For i = 0 To 9
+                box(0, i).Visible = (i <= index)
+                box(1, i).Visible = (i <= index)
+                lblText(i).Visible = (i <= index)
+            Next
+        Else
+            For i = 0 To 9
+                box(0, i).Visible = False
+                box(1, i).Visible = False
+                lblText(i).Visible = False
+            Next
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim N As Integer = Val(TextBox1.Text) - 1
+        If N < 2 Or N > 9 Then MsgBox("輸入的矩陣數必須在3~10之間。", MsgBoxStyle.OkOnly, "提示") : Exit Sub
+        Dim R(N) As Rect
+        Dim Record As String = ""
+        Dim BestTimes As Integer = Integer.MaxValue
+        '//Get Value
+        For i = 0 To N
+            R(i).X = Val(box(0, i).Text)
+            R(i).Y = Val(box(1, i).Text)
+            R(i).Num = i + 1
+        Next
+        '//DFS
+        Dim newRect() As Rect : Dim Pos As Integer
+        For i = 0 To N - 1
+            For q = i + 1 To N
+                If R(i).Y = R(q).X Then
+                    Dim NowRect As Rect : NowRect.Setting(R(i).X, R(q).Y, -1)
+                    ReDim newRect(N - 2) : Pos = -1
+                    For k = 0 To N
+                        If k <> i And k <> q Then
+                            Pos += 1
+                            newRect(Pos) = R(k)
+                        End If
+                    Next
+                    DFS(BestTimes, Record, newRect, NowRect, R(i).X * R(i).Y * R(q).Y, "<m" & R(i).Num & " m" & R(q).Num & ">")
+                ElseIf R(i).X = R(q).Y Then
+                    Dim NowRect As Rect : NowRect.Setting(R(i).Y, R(q).X, -1)
+                    ReDim newRect(N - 2) : Pos = -1
+                    For k = 0 To N
+                        If k <> i And k <> q Then
+                            Pos += 1
+                            newRect(Pos) = R(k)
+                        End If
+                    Next
+                    DFS(BestTimes, Record, newRect, NowRect, R(i).Y * R(i).X * R(q).X, "<m" & R(i).Num & " m" & R(q).Num & ">")
+                End If
+            Next
+        Next
+        '//Output
+        Label4.Text = "矩陣相乘的次序為: " & Record
+        Label3.Text = "最少的乘法運算次數: " & BestTimes
+    End Sub
+    Sub DFS(ByRef BestTimes As Integer, ByRef Record As String, ByVal Rects() As Rect, ByVal NowRect As Rect, ByVal Times As Integer, ByVal Recording As String)
+        If IsNothing(Rects) Then
+            If Times < BestTimes Then
+                BestTimes = Times
+                Record = Recording
+            End If
+        Else
+            For i = 0 To UBound(Rects)
+                If NowRect.Y = Rects(i).X Then
+                    Dim New_NowRect As Rect : New_NowRect.Setting(NowRect.X, Rects(i).Y, -1)
+                    Dim newRects() As Rect : Dim Pos As Integer = -1
+                    For k = 0 To UBound(Rects)
+                        If k <> i Then
+                            Pos += 1
+                            ReDim Preserve newRects(Pos)
+                            newRects(Pos) = Rects(k)
+                        End If
+                    Next
+                    DFS(BestTimes, Record, newRects, New_NowRect, Times + NowRect.X * NowRect.Y * Rects(i).Y, "<" & Recording & " m" & Rects(i).Num & ">")
+                ElseIf NowRect.X = Rects(i).Y Then
+                    Dim New_NowRect As Rect : New_NowRect.Setting(NowRect.Y, Rects(i).X, -1)
+                    Dim newRects() As Rect : Dim Pos As Integer = -1
+                    For k = 0 To UBound(Rects)
+                        If k <> i Then
+                            Pos += 1
+                            ReDim Preserve newRects(Pos)
+                            newRects(Pos) = Rects(k)
+                        End If
+                    Next
+                    DFS(BestTimes, Record, newRects, New_NowRect, Times + NowRect.Y * NowRect.X * Rects(i).X, "<" & Recording & " m" & Rects(i).Num & ">")
+                End If
+            Next
+        End If
+    End Sub
+End Class
+Public Structure Rect
+    Public X As Integer
+    Public Y As Integer
+    Public Num As Integer
+    Public Sub Setting(Xr As Integer, Yr As Integer, Num As Integer)
+        X = Xr : Y = Yr
+    End Sub
+End Structure
