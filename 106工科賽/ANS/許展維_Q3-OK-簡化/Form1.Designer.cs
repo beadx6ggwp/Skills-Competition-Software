7c84452bc048b171fb301170b286e504e1afb7ca
DavidHsu <davidmd9830415@gmail.com>
2018-05-14 20:09:45 +0800

    update

---
+++ 106工科賽/ANS/許展維_Q3-OK-簡化/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace 許展維_Q3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(127, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(60, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 27);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(28, 121);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(27, 60);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(27, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(27, 60);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(60, 98);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 27);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button8.Location = new System.Drawing.Point(196, 183);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(113, 41);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Random Set";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button9.Location = new System.Drawing.Point(196, 230);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(113, 41);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Recognizing";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(25, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "數字為:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(106, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 287);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 106工科賽/ANS/許展維_Q3-OK-簡化/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 許展維_Q3
+{
+    public partial class Form1 : Form
+    {
+        Button[] btn;
+        int[] nowCheck = new int[7];
+        List<LED> list = new List<LED>();
+
+        Random ran = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            btn = new Button[] { button1, button2, button3, button4, button5, button6, button7 };
+            foreach (var item in btn)
+            {
+                item.Text = "";
+                item.Click += (s, e) =>
+                {
+                    int index = Array.IndexOf(btn, (Button)s);
+                    nowCheck[index] = (nowCheck[index] + 1) % 2;
+                    setLED(nowCheck);
+                };
+            }
+
+            setLED(nowCheck);
+
+            list.Add(new LED(0, new int[] { 1, 1, 1, 1, 1, 1, 0 }));
+            list.Add(new LED(1, new int[] { 0, 1, 1, 0, 0, 0, 0 }));
+            list.Add(new LED(1, new int[] { 0, 0, 0, 0, 1, 1, 0 }));
+            list.Add(new LED(2, new int[] { 1, 1, 0, 1, 1, 0, 1 }));
+            list.Add(new LED(3, new int[] { 1, 1, 1, 1, 0, 0, 1 }));
+            list.Add(new LED(4, new int[] { 0, 1, 1, 0, 0, 1, 1 }));
+            list.Add(new LED(5, new int[] { 1, 0, 1, 1, 0, 1, 1 }));
+            list.Add(new LED(6, new int[] { 1, 0, 1, 1, 1, 1, 1 }));
+            list.Add(new LED(6, new int[] { 0, 0, 1, 1, 1, 1, 1 }));
+            list.Add(new LED(7, new int[] { 1, 1, 1, 0, 0, 0, 0 }));
+            list.Add(new LED(8, new int[] { 1, 1, 1, 1, 1, 1, 1 }));
+            list.Add(new LED(9, new int[] { 1, 1, 1, 1, 0, 1, 1 }));
+            list.Add(new LED(9, new int[] { 1, 1, 1, 0, 0, 1, 1 }));
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++) nowCheck[i] = ran.Next(0, 2);
+            setLED(nowCheck);
+            check();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            check();
+        }
+        void check()
+        {
+            foreach (var item in list)
+            {
+                int count = 0;
+                for (int i = 0; i < 7; i++)
+                {
+                    if (item.arr[i] == nowCheck[i])
+                        count++;
+                }
+
+                if (count == 7)
+                {
+                    label2.Text = item.num.ToString();
+                    return;
+                }
+            }
+            label2.Text = "非數字";
+        }
+
+        void setLED(int[] arr)
+        {
+            for (int i = 0; i < 7; i++)
+                nowCheck[i] = arr[i];
+
+            for (int i = 0; i < 7; i++)
+                btn[i].BackColor = (arr[i] == 1) ? Color.Black : Color.White;
+        }
+    }
+
+    class LED
+    {
+        public int num;
+        public int[] arr = new int[7];
+        public LED(int n, int[] _arr)
+        {
+            num = n;
+            for (int i = 0; i < 7; i++)
+                arr[i] = _arr[i];
+        }
+    }
+}
